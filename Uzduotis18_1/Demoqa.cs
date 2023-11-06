4692963021397e7328437a3bf9c16c7809fc46e4
VilmaDediniene <vilmasestokaite1@gmail.com>
2023-11-06 12:07:00 +0200

    Add homework

---
+++ Uzduotis18_1/Demoqa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Uzduotis18_1
+{
+    public class Demoqa
+    {
+        [Test]
+
+        public static void Main()
+        {
+            string expectedResult = "Name:Jonas Jonauskas\r\n\r\nEmail:jonas.jonauskas@gmail.com\r\n\r\nCurrent Address :Kernavės g. 20, Vilnius\r\n\r\nPermananet Address :Taikos g. 2, Neringa";
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://demoqa.com/text-box";
+
+            IWebElement inputFullName = driver.FindElement(By.XPath("//*[@id=\"userName\"]"));
+            inputFullName.SendKeys("Jonas Jonauskas");
+
+            IWebElement inputEmail = driver.FindElement(By.XPath("//*[@id=\"userEmail\"]"));
+            inputEmail.SendKeys("jonas.jonauskas@gmail.com");
+
+            IWebElement inputCurrentAddress = driver.FindElement(By.XPath("//*[@id=\"currentAddress\"]"));
+            inputCurrentAddress.SendKeys("Kernavės g. 20, Vilnius");
+
+            IWebElement inputPermanentAddress = driver.FindElement(By.XPath("//*[@id=\"permanentAddress\"]"));
+            inputPermanentAddress.SendKeys("Taikos g. 2, Neringa");
+
+            IWebElement buttonSubmit = driver.FindElement(By.XPath("//*[@id=\"submit\"]"));
+            buttonSubmit.Click();
+
+            string actualResult = driver.Title;
+
+            driver.Quit();
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+    }
+}
